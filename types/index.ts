280c6ae084a463dca3bb1ae99ba493c875802f47
oskvr37 <oskarxpd@gmail.com>
2023-05-12 23:06:47 +0200

    add types & sass

---
+++ types/index.ts
@@ -0,0 +1,30 @@
+interface TCar {
+  id: number;
+  img: string;
+  name: string;
+  year: number;
+  make: string;
+  category: string;
+  price: number;
+  sources: string[];
+  score: number;
+  transmission: string;
+  power: number;
+  weight: number;
+  stats: {
+    speed: number;
+    handling: number;
+    acceleration: number;
+    launch: number;
+    braking: number;
+    offroad: number;
+  };
+  perfs: {
+    top_speed: number;
+    to60: number;
+    to100: number;
+    lateral_g: number;
+  };
+}
+
+export type { TCar }
