a26c6956eab045170d04e7c91c6b408cc8a15885
oskvr37 <oskarxpd@gmail.com>
2023-05-13 21:56:36 +0200

    design tweaks

--- components/Car.tsx
+++ components/Car.tsx
@@ -23,29 +23,34 @@ export default function Car({ car }: { car: TCar }) {
   return (
     <div className={style.container}>
       <div className={style.heading}>
-        <div className={style.photo}>
-          <img src={`https://www.kudosprime.com/fh5/${img}`} alt={name} />
+        <div className={style.row}>
+          <div className={style.photo}>
+            <img src={`https://www.kudosprime.com/fh5/${img}`} alt={name} />
+          </div>
+          <div className={style.basic}>
+            <p>
+              <i>{year}</i> {make}
+            </p>
+            <h3>{name}</h3>
+            <p>{category}</p>
+          </div>
         </div>
-        <div className={style.basic}>
-          <p>
-          <i>{year}</i> {make}
-          </p>
-          <h3>{name}</h3>
-          <p>{category}</p>
+        <div>
+          <Pi pi={score} />
         </div>
-        <Pi pi={score} />
       </div>
 
-      <p className={style.price}>{price.toLocaleString("en-US")} Cr</p>
-      
-      <p>{transmission}</p>
+        <p className={style.price}>{price.toLocaleString("en-US")} Cr</p>
+      {/* <p>{transmission}</p>
       <p>{power} HP</p>
-      <p>{weight} kg</p>
-      <div className={style.sources}>
+      <p>{weight} kg</p> */}
+
+      {/* <div className={style.sources}>
         {sources.map((source) => (
           <span key={source}>{source}</span>
         ))}
-      </div>
+      </div> */}
+
     </div>
   );
 }
